af392a7fd1e7dfadf386796aec862c5731c41582
miniece <mvelinda@icloud.com>
2022-11-18 14:07:38 -0500

    adding sql prompts

---
+++ SQLPrompts.sql
@@ -0,0 +1,22 @@
+--create database Upmeet;
+
+--create table Events(
+--id int primary key identity (1,1),
+--[Name] nvarchar(50),
+--[Date/Time] datetime,
+--[Location] nvarchar(50),
+--[Description] nvarchar(350)
+--);
+
+--create table Favorites(
+--EventsID int foreign key references Events(id)
+--);
+
+--insert into Events
+--values('Uncanny Hootenanny', '2023/01/01 00:00:00', 'Log Cabin on 63rd', 'Kick off the yee haw and strap your boots up for the 1st Annual Uncanny Hootenanny of your dreams. Spots are limited so register now!'),
+--('BYOBooty Dance Party', '2023/02/14 19:30:00', 'The Lovers Quarrel Bar', 'Come to this here party an dance wit yer mateys or bunker down wit yer wenches or menches.'),
+--('All-Weather Get-Together', '2022/12/15 14:30:00', 'Tommys New House', 'Come disciples and gather to celebrate our might and savior, Shrek.'),
+--('Potluck Palooza', '2023/04/13', 'The Bus Stop on 3rd Avenue', 'Bring only your best dishes to share with everyone. No poison allowed. We legally need this disclaimer because of the event that happened last year.'),
+--('Existential End Excursion', '2023/05/21 00:05:21', 'A Cornfield', 'Reflect on the end and prepare for the worst. Snacks will be provided.');
+
+--select * from Events
